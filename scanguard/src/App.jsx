1d9205ead794afbe3b54970b848f05f3ab9389c1
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-04-23 20:28:38 +0300

    add routes and update path

--- scanguard/src/App.jsx
+++ scanguard/src/App.jsx
@@ -1,10 +1,17 @@
 import "./Styles/utilities.css"
 import Authentication from "./Authentication";
+import Home from "./Home";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Authentication />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/auth" element ={<Authentication />}/>
+          <Route path="/" element ={<Home />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
