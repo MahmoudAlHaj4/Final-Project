14ba24c17e95c744ef8d752b3b9c2d4bf87b2e95
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-04-27 16:50:14 +0300

    add signup container

--- scanguard/src/Authentication/index.jsx
+++ scanguard/src/Authentication/index.jsx
@@ -1,7 +1,19 @@
 
 const Authentication  =  ()=>{
-    return (
-        <div>Hello Auth</div>
+    return(
+     
+        <Components.Container>
+            <Components.SignUpContainer>
+                <Components.Form>
+                    <Components.Title>Create Account</Components.Title>
+                    <Components.Input type='text' placeholder='Name' />
+                    <Components.Input type='email' placeholder='Email' />
+                    <Components.Input type='password' placeholder='Password' />
+                    <Components.Button>Sign Up</Components.Button>
+                </Components.Form>
+            </Components.SignUpContainer>
+        </Components.Container>
+        
     )
 }
 
