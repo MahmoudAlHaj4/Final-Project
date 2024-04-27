5076970a36d0dad3d95d54003c4cc352f9797fc7
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-04-27 22:07:58 +0300

    add tabs

--- scanguard/src/Home/index.jsx
+++ scanguard/src/Home/index.jsx
@@ -26,6 +26,18 @@ const Home = ()=>{
                 <button>Get Started</button>
             </div>
         </div>
+        <div class="tabs">
+
+            <input type="radio" class="tabs__radio" name="tabs-example" id="tab1" checked />
+            <label for="tab1" class="tabs__label">Overview</label>
+            <div class="tabs__content flex ">
+            </div>
+
+            <input type="radio" class="tabs__radio" name="tabs-example" id="tab2" />
+            <label for="tab2" class="tabs__label">Identity Verification</label>
+            <div class="tabs__content">       
+            </div>
+        </div>
            
     </div>
 }
