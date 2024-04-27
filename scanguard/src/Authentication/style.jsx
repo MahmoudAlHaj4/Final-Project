85f73a29409eca1f2e15fe1c668704cda938c4af
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-04-27 17:01:40 +0300

    style signup container

--- scanguard/src/Authentication/style.jsx
+++ scanguard/src/Authentication/style.jsx
@@ -17,3 +17,19 @@ export const Wrapper = styled.div`
   align-items: center;
 `;
 
+export const SignUpContainer = styled.div`
+position: absolute;
+top: 0;
+height: 100%;
+transition: all 0.6s ease-in-out;
+left: 0;
+width: 50%;
+opacity: 0;
+z-index: 1;
+${props => props.signinIn !== true ? `
+  transform: translateX(100%);
+  opacity: 1;
+  z-index: 5;
+` 
+: null}
+`;
