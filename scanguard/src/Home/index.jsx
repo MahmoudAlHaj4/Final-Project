1cd8f203abc412aa8fba4867797f169e6877fcaa
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-04-27 16:05:42 +0300

    navigate to auth page

--- scanguard/src/Home/index.jsx
+++ scanguard/src/Home/index.jsx
@@ -1,6 +1,7 @@
 import "./home.css"
-
+import { useNavigate } from "react-router-dom";
 const Home = ()=>{
+    const navigate = useNavigate()
     return <div className="home-page">
         <div className="navbar flex center">
             <div className="logo">
@@ -12,8 +13,8 @@ const Home = ()=>{
                 <button><span>Contact Us</span></button>
             </div>
             <div className="btns">
-                <button>SignIn</button>
-                <button>SignUp</button>
+                <button onClick={()=>navigate('/auth')}>SignIn</button>
+                <button onClick={()=>navigate('/auth')}>SignUp</button>
             </div>
         </div>
         <div className="hero"></div>
