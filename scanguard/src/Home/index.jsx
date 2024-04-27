16403ef89e8631dd034c82c676bc4080e8c68583
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-04-27 16:04:27 +0300

    add navbar

--- scanguard/src/Home/index.jsx
+++ scanguard/src/Home/index.jsx
@@ -1,7 +1,22 @@
+import "./home.css"
 
 const Home = ()=>{
-    return <div>
-        hello Home
+    return <div className="home-page">
+        <div className="navbar flex center">
+            <div className="logo">
+                <h2>ScanGuard</h2>
+            </div>
+            <div className="links flex ">
+                <button><span>Home</span></button>
+                <button><span>Service</span></button>
+                <button><span>Contact Us</span></button>
+            </div>
+            <div className="btns">
+                <button>SignIn</button>
+                <button>SignUp</button>
+            </div>
+        </div>
+        <div className="hero"></div>
     </div>
 }
 
